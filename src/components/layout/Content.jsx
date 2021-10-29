fa72aece63bf9f2405d583a1428a4c0b6ae8d02e
black.dev.x <thiago.dev.x.@gmail.com>
2021-10-29 16:08:12 -0300

    basic routing

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -1,5 +1,19 @@
 import './Content.css'
 import React from 'react'
-const Content = () => <div className='Content'>Content</div>
+import Home from '../../views/Home'
+import About from '../../views/About'
+import { Route, Switch } from 'react-router'
+const Content = () => (
+  <div className='Content'>
+    <Switch>
+      <Route path='/'>
+        <Home />
+      </Route>
+      <Route path='/about'>
+        <About />
+      </Route>
+    </Switch>
+  </div>
+)
 
 export default Content
--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Menu from '../components/layout/Menu'
 import Content from '../components/layout/Content'
+import { BrowserRouter as Router } from 'react-router-dom'
 import './App.css'
 
 const App = () => (
   <div className='App'>
-    <Menu />
-    <Content />
+    <Router>
+      <Menu />
+      <Content />
+    </Router>
   </div>
 )
 
